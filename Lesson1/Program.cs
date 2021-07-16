27b7c3c4d0a3fa04f972b9ed231a710e7ebe637c
Erik <Erik@DESKTOP-FFKLVQ5>
2021-07-16 20:03:11 +0500

    Добавьте файлы проекта.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько вам лет?");
+            string bas;//*объявление переменной строкового типа
+            bas = Console.ReadLine();//инициализация переменной (присвоение значения)
+            int age;//*объявление переменной целочисленного типа
+            age = int.Parse(bas);
+            if (age >= 18) //если что-то
+            {
+                Console.WriteLine("Вы взрослый");
+            }
+            else
+            {
+                Console.WriteLine("Вы король");
+            }
+        }
+    }
+}
